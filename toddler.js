82ddf1db3a88005425e3a1ecf36aba41607ab552
OyungerelA <79796327+OyungerelA@users.noreply.github.com>
2021-06-22 02:02:10 +0800

    Update toddler.js

--- toddler.js
+++ toddler.js
@@ -32,6 +32,7 @@ option1.addEventListener("click",()=>{
 document.getElementById("option1").addEventListener("click", ()=>{
 	document.getElementById("page3").style.display="block"
 	document.getElementById("page2").style.display="none"
+	// document.getElementById("audio").stop()
 
 })
 
@@ -42,6 +43,8 @@ option2.addEventListener("click",()=>{
 document.getElementById("option2").addEventListener("click", ()=>{
 	document.getElementById("page4").style.display="block"
 	document.getElementById("page2").style.display="none"
+	// document.getElementById("audio").stop()
+	// document.getElementById("page3").style.display="none"
 })
 
 var dynamic2 = document.getElementById("dynamic2")
@@ -60,30 +63,73 @@ audio3.addEventListener("ended", ()=>{
 document.getElementById("option21").addEventListener("click", ()=>{
 	document.getElementById("page5").style.display="block"
 	document.getElementById("page3").style.display="none"
-
+	// document.getElementById("audio2").stop()
+	document.getElementById("audio4").play()
 })
 
 document.getElementById("option22").addEventListener("click", ()=>{
 	document.getElementById("page6").style.display="block"
 	document.getElementById("page3").style.display="none"
+	// document.getElementById("audio2").stop()
+	document.getElementById("audio5").play()
 })
 
 document.getElementById("option221").addEventListener("click", ()=>{
 	document.getElementById("page5").style.display="block"
-	document.getElementById("page3").style.display="none"
-
+	document.getElementById("page4").style.display="none"
+	// document.getElementById("audio3").stop()
+	document.getElementById("audio4").play()
 })
 
 document.getElementById("option222").addEventListener("click", ()=>{
 	document.getElementById("page6").style.display="block"
-	document.getElementById("page3").style.display="none"
+	document.getElementById("page4").style.display="none"
+	// document.getElementById("audio3").stop()
+	document.getElementById("audio5").play()
+})
 
+// document.getElementById("audio4").addEventListener("ended",()=>{
+// 	document.getElementById("page5").style.display="none"
+// 	document.getElementById("page6").style.display="block"
+// 	document.getElementById("audio5").play()
+// })
+
+document.getElementById("audio4").addEventListener("ended",()=>{
+	document.getElementById("dynamic4").style.display="block"
+})
+
+document.getElementById("audio5").addEventListener("ended",()=>{
+	document.getElementById("dynamic5").style.display="block"
+})
+
+document.getElementById("option31").addEventListener("click", ()=>{
+	document.getElementById("page7").style.display="block"
+	document.getElementById("page5").style.display="none"
+	// document.getElementById("audio4").stop()
+	document.getElementById("audio6").play()
+})
+
+document.getElementById("option32").addEventListener("click", ()=>{
+	document.getElementById("page8").style.display="block"
+	document.getElementById("page5").style.display="none"
+	// document.getElementById("audio4").stop()
+	document.getElementById("audio7").play()
+})
+
+document.getElementById("option41").addEventListener("click", ()=>{
+	document.getElementById("page7").style.display="block"
+	document.getElementById("page6").style.display="none"
+	// document.getElementById("audio5").stop()
+	document.getElementById("audio6").play()
+})
+
+document.getElementById("option42").addEventListener("click", ()=>{
+	document.getElementById("page8").style.display="block"
+	document.getElementById("page6").style.display="none"
+	// document.getElementById("audio5").stop()
+	document.getElementById("audio7").play()
 })
 
 // option1.addEventListener("click",=>{
 // 	audio.play()
 // })
-
-
-
-
