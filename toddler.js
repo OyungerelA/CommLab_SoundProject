82778c87552a26948729cf01c2e5e564d4d08ac5
OyungerelA <79796327+OyungerelA@users.noreply.github.com>
2021-06-22 17:16:34 +0800

    Update toddler.js

--- toddler.js
+++ toddler.js
@@ -1,24 +1,9 @@
 var dynamic = document.getElementById("dynamic")
 var audio = document.getElementById("audio")
 
-// elem.addEventListener("click",()=>{
-// 	// element.style.display = "block"
-// 	audio.play()
-// })
-
-// var page2 = document.getElementById("page2")
-
-// page2.addEventListener("onmouseover",()=>{
-// 	audio.play()
-// })
-
 audio.addEventListener("ended", ()=>{
 	dynamic.style.display = "block"
 })
-
-
-
-
 
 var option1 = document.getElementById("option1")
 var audio2 = document.getElementById("audio2")
@@ -32,7 +17,6 @@ option1.addEventListener("click",()=>{
 document.getElementById("option1").addEventListener("click", ()=>{
 	document.getElementById("page3").style.display="block"
 	document.getElementById("page2").style.display="none"
-	// document.getElementById("audio").stop()
 	document.getElementById("audio").pause()
 })
 
@@ -43,18 +27,14 @@ option2.addEventListener("click",()=>{
 document.getElementById("option2").addEventListener("click", ()=>{
 	document.getElementById("page4").style.display="block"
 	document.getElementById("page2").style.display="none"
-	// document.getElementById("audio").stop()
-	// document.getElementById("page3").style.display="none"
 	document.getElementById("audio").pause()
 })
 
 var dynamic2 = document.getElementById("dynamic2")
 var dynamic3 = document.getElementById("dynamic3")
 
-
 audio2.addEventListener("ended", ()=>{
 	dynamic2.style.display = "block"
-
 })
 
 audio3.addEventListener("ended", ()=>{
@@ -64,36 +44,26 @@ audio3.addEventListener("ended", ()=>{
 document.getElementById("option21").addEventListener("click", ()=>{
 	document.getElementById("page5").style.display="block"
 	document.getElementById("page3").style.display="none"
-	// document.getElementById("audio2").stop()
 	document.getElementById("audio4").play()
 })
 
 document.getElementById("option22").addEventListener("click", ()=>{
 	document.getElementById("page6").style.display="block"
 	document.getElementById("page3").style.display="none"
-	// document.getElementById("audio2").stop()
 	document.getElementById("audio5").play()
 })
 
 document.getElementById("option221").addEventListener("click", ()=>{
 	document.getElementById("page5").style.display="block"
 	document.getElementById("page4").style.display="none"
-	// document.getElementById("audio3").stop()
 	document.getElementById("audio4").play()
 })
 
 document.getElementById("option222").addEventListener("click", ()=>{
 	document.getElementById("page6").style.display="block"
 	document.getElementById("page4").style.display="none"
-	// document.getElementById("audio3").stop()
 	document.getElementById("audio5").play()
 })
-
-// document.getElementById("audio4").addEventListener("ended",()=>{
-// 	document.getElementById("page5").style.display="none"
-// 	document.getElementById("page6").style.display="block"
-// 	document.getElementById("audio5").play()
-// })
 
 document.getElementById("audio4").addEventListener("ended",()=>{
 	document.getElementById("dynamic4").style.display="block"
@@ -106,21 +76,18 @@ document.getElementById("audio5").addEventListener("ended",()=>{
 document.getElementById("option31").addEventListener("click", ()=>{
 	document.getElementById("page7").style.display="block"
 	document.getElementById("page5").style.display="none"
-	// document.getElementById("audio4").stop()
 	document.getElementById("audio6").play()
 })
 
 document.getElementById("option32").addEventListener("click", ()=>{
 	document.getElementById("page8").style.display="block"
 	document.getElementById("page5").style.display="none"
-	// document.getElementById("audio4").stop()
 	document.getElementById("audio7").play()
 })
 
 document.getElementById("option41").addEventListener("click", ()=>{
 	document.getElementById("page7").style.display="block"
 	document.getElementById("page6").style.display="none"
-	// document.getElementById("audio5").stop()
 	document.getElementById("audio6").play()
 })
 
@@ -131,14 +98,9 @@ document.getElementById("audio6").addEventListener("ended",()=>{
 document.getElementById("option42").addEventListener("click", ()=>{
 	document.getElementById("page8").style.display="block"
 	document.getElementById("page6").style.display="none"
-	// document.getElementById("audio5").stop()
 	document.getElementById("audio7").play()
 })
 
 document.getElementById("audio7").addEventListener("ended",()=>{
 	document.getElementById("info2").style.display="block"
 })
-
-// option1.addEventListener("click",=>{
-// 	audio.play()
-// })
